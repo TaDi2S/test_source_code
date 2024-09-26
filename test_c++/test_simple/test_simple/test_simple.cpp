4e580633736b931072fe69ef2e24f1579937e0a3
TaDi2S <ehdbs678@naver.com>
2024-09-26 14:25:16 +0900

    simpletest 추가

---
+++ test_c++/test_simple/test_simple/test_simple.cpp
@@ -0,0 +1,11 @@
+﻿// test_simple.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int main()
+{
+	char* a = NULL;
+	printf("NOTTING=%s,\\0=%s,\"\"=%s,NULL=%s,nullptr=%s\n", (char *)"NOTTHING", '\0', "", NULL, nullptr);
+	printf("NOTTING=%s,\\0=%s,\"\"=%s,NULL=%s,nullptr=%s\n", (char *)"NOTTHING",'\0',"", a ? a : "" ,nullptr);
+}
